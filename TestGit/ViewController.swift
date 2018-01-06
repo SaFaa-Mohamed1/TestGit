901923e42a7c512ff57f15e733a10d387cc24963
SaFaa Mohamed <SaFaa@Bahaas-MacBook-Pro.local>
2018-01-06 20:45:48 +0200

    Initial Commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by SaFaa Mohamed on 1/6/18.
+//  Copyright © 2018 SaFaa Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
